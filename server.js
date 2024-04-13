6b28bdd97043a3030d0736a18b45d531cb88df27
daltonoswald <daltonoswald@gmail.com>
2024-04-13 11:26:33 -0400

    all files completed

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+    let q = url.parse(req.url, true);
+    let filename = "";
+    if (q.pathname === "/") {
+        filename = "./index.html"
+    } else {
+        filename = "." + q.pathname + ".html";
+    }
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            fs.readFile('./404.html', function(err, data) {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+            })
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        }
+    })
+}).listen(8080)
